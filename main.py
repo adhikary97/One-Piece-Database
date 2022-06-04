882218248108f223da4f4d8c78113250a25651e4
Paras Adhikary <paras.adhikary@salesforce.com>
2022-06-03 17:04:08 -0700

    Updated readme, finished text extraction, automated extraction

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ config = ('-l eng --oem 1 --psm 3')
 # set path to tesseract
 pytesseract.pytesseract.tesseract_cmd = '/usr/local/Cellar/tesseract/5.1.0/bin/tesseract'
 
+def return_txt(filename):
+	return filename.replace('.jpg', '.txt').replace('.jpeg', '.txt').replace('.png', '.txt')
 
 def text_extraction(image_file):
 	# read image
 	img = cv2.imread(image_file)
-	file_name = image_file.split('.')[0]
+	file_name = return_txt(image_file)
 
 	text = pytesseract.image_to_string(img, config=config)
 
-	with open(f'{file_name}.txt', 'w+') as fil:
+	with open(file_name, 'w+') as fil:
 		fil.write(text)
 		print(text)
 
@@ -25,11 +27,11 @@ def main(args):
 		print('Invalid filname passed, file must end with .jpg, .jpeg, .png')
 		sys.exit(1)
 	
-	print(f'Extracting text from "{args.file}", text output "{args.file.split(".")[0]}.txt"')
+	print(f'Extracting text from "{args.file}", text output "{return_txt(args.file)}"')
 	print('------------------------')
 	text_extraction(args.file)
 	print('------------------------')
-	print('Text extraction complete')
+	print('Text extraction complete\n')
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Extract text from provided image.')
